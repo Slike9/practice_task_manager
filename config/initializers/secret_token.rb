a7e5c75b208e84461f4869c57bfb7638c1c48420
Sergey Semyonov <slike9@rambler.ru>
2013-09-26 11:21:50 +0400

    rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PracticeTaskManager::Application.config.secret_key_base = 'd9a7fbd47ba02484957f56a32f4ad435d9f876d9f0c378421d96e373a88ed19a7368296a1a3ec65dc7b5299dd9c17124b2d0e441bc7544f03139d720d8f27402'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PracticeTaskManager::Application.config.session_store :cookie_store, key: '_practice_task_manager_session'
