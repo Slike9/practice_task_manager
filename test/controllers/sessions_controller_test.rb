528eac14e3184373646a0e55db12145fc22b0060
Sergey Semyonov <slike9@rambler.ru>
2013-09-27 21:01:36 +0400

    SessionsController tests renamed

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,24 +5,24 @@ class SessionsControllerTest < ActionController::TestCase
     @present_user = FactoryGirl.create(:user)
   end
 
-  test "get new success" do
+  test "get new" do
     get :new
     assert_response :success
   end
 
-  test 'post create with correct user data signs in the user' do
+  test 'creating user session: user data correct' do
     post :create, user_data: {email: @present_user.email, password: @present_user.password}
     assert_response :redirect
     assert_equal @present_user, @controller.current_user
   end
 
-  test 'post create with incorrect user data: any user does not sign in' do
+  test 'creating user session: user data incorrect' do
     post :create, user_data: {email: '2@2.ru', password: '2'}
     assert_response :success
     assert !@controller.user_signed_in?
   end
 
-  test 'delete destroy signs out user' do
+  test 'destroying user session' do
     @controller.sign_in(@present_user)
     delete :destroy
     assert_response :redirect
