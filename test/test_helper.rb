6eeb51107b0579e8d1aa1ee6737708fe93700e9b
Sergey Semyonov <slike9@rambler.ru>
2013-09-27 16:13:54 +0400

    coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,12 @@
 ENV["RAILS_ENV"] ||= "test"
+if ENV['TRAVIS']
+  require 'coveralls'
+  Coveralls.wear!
+else
+  require 'simplecov'
+  SimpleCov.start
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
