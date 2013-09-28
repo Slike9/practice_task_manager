1e39496f6ee8e6bd2f8ad1d58d523f0796284ea5
Sergey Semyonov <slike9@rambler.ru>
2013-09-28 10:58:07 +0400

    StoryStates: states listed

--- app/models/concerns/story_states.rb
+++ app/models/concerns/story_states.rb
@@ -3,6 +3,12 @@ module StoryStates
 
   included do
     state_machine :state, initial: :new do
+      state :new
+      state :started
+      state :finished
+      state :accepted
+      state :rejected
+
       event :start do
         transition :new => :started
       end
