55ba4feb144b140252c7724330079399e3dd6e5e
Sergey Semyonov <slike9@rambler.ru>
2013-12-19 20:26:32 +0400

    t

--- app/controllers/web/images_controller.rb
+++ app/controllers/web/images_controller.rb
@@ -2,12 +2,13 @@ class Web::ImagesController < ApplicationController
   def show
     sleep params[:sleep].to_f if params[:sleep]
     
+    im = params[:im] || '1.png'
     respond_to do |format|
       format.png do
-        send_file Rails.root.join('public/404.html'), type: 'image/png', disposition: 'inline'
+        send_file Rails.root.join("public/#{im}"), type: 'image/png', disposition: 'inline'
       end
       format.jpeg do
-        send_file Rails.root.join('public/404.html'), type: 'image/jpeg', disposition: 'inline'
+        send_file Rails.root.join("public/#{im}"), type: 'image/jpeg', disposition: 'inline'
       end
     end
   end
