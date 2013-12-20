55a547a0026f1a27df1bb49406febc1c9fcbba31
Sergey Semyonov <slike9@rambler.ru>
2013-12-20 10:25:45 +0400

    t

--- app/controllers/web/images_controller.rb
+++ app/controllers/web/images_controller.rb
@@ -3,13 +3,17 @@ class Web::ImagesController < ApplicationController
     sleep params[:sleep].to_f if params[:sleep]
     
     im = params[:im] || '1.png'
+    file = "public/#{im}"
+    t_file = 'public/im.png'
+    FileUtils.cp file, t_file
     respond_to do |format|
       format.png do
-        send_file Rails.root.join("public/#{im}"), type: 'image/png', disposition: 'inline'
+        send_file Rails.root.join(t_file), type: 'image/png', disposition: 'inline'
       end
       format.jpeg do
-        send_file Rails.root.join("public/#{im}"), type: 'image/jpeg', disposition: 'inline'
+        send_file Rails.root.join(t_file), type: 'image/jpeg', disposition: 'inline'
       end
     end
+    FileUtils.rm(t_file)
   end
 end
