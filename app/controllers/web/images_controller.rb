ca0d5c74eb92d9b6640347df27126831b16afd0c
Sergey Semyonov <slike9@rambler.ru>
2013-12-20 12:14:35 +0400

    t

--- app/controllers/web/images_controller.rb
+++ app/controllers/web/images_controller.rb
@@ -5,15 +5,20 @@ class Web::ImagesController < ApplicationController
     im = params[:im] || '1.png'
     file = "public/#{im}"
     t_file = 'public/im.png'
+    t = Tempfile.new('t')
+    t_file = t.path
     FileUtils.cp file, t_file
     respond_to do |format|
       format.png do
+        puts '11111'
         send_file Rails.root.join(t_file), type: 'image/png', disposition: 'inline'
+        #FileUtils.rm(t_file)
+        puts '2222222'
       end
       format.jpeg do
         send_file Rails.root.join(t_file), type: 'image/jpeg', disposition: 'inline'
+        #FileUtils.rm(t_file)
       end
     end
-    FileUtils.rm(t_file)
   end
 end
